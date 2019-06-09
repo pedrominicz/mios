e4c39d55ef5c0aee8fead7ec9e84010f1c52d73e
Pedro Minicz <pedrominicz@gmail.com>
2019-06-09 17:56:14 -0300

    add print routines

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 `chmod +x run` and `./run` to run.
+
+### useful resources
+
+http://www.brokenthorn.com/Resources/OSDev3.html
+
+http://www.brokenthorn.com/Resources/OSDev4.html
+
+https://github.com/jhu-cs318/pintos/blob/master/src/threads/loader.S
+
+http://stanislavs.org/helppc/int_table.html
--- boot.asm
+++ boot.asm
@@ -1,26 +1,66 @@
+;;;; bootloader
+
+;;;; the BIOS loads this 512 byte code at address 0x7c00 and jumps to it in
+;;;; real mode, as such, this code should be located in the first 512 bytes of
+;;;; a boot device
+
 org     0x7c00                  ; the BIOS loads the bootloader at 0x7c00
-bits    16
+bits    16                      ; 16-bit real mode
 
 start:
         xor     ax, ax
-        mov     ds, ax
-        cld                     ; clear direction flag, when the DF is 0,
-                                ; string operations increment the index
-                                ; registers
-        mov     si, msg
-printloop:
-        lodsb                   ; load string byte: load byte at dress DS:SI
-                                ; into AL and increments/decrements SI
-        or      al, al
-        jz      hang            ; jump if AL is zero
-        mov     ah, 0x0e
-        int     0x10            ; BIOS video interrupt
-        jmp     printloop
+        mov     ds, ax          ; data segment register
+        mov     ss, ax          ; stack segment register
+        mov     esp, 0xf000
+
+;;;; initialize serial port 0 with BIOS interrupt 0x14,0, so we can report
+;;;; status even without VGA, note that AH was zeroed
+        xor     dx, dx
+        mov     al, 0b11100011
+        int     0x14            ; http://stanislavs.org/helppc/int_14-0.html
+
+        call    puts
+        db      'mios', 10, 0
+        call    puts
+        db      'hello os world', 10, 0
+
 hang:
         cli                     ; clear interrupt flag
         hlt
+        jmp     hang
 
-msg     db      'hello os world', 0
+;;;; print null-terminated string located right after the call instruction
+puts:
+        xchg    [esp], si
+        push    ax
+.1:     mov     al, [si]
+        inc     si
+        test    al, al
+        jz      .2
+        call    putc
+        jmp     .1
+.2:     pop     ax
+        xchg    [esp], si
+        ret
+
+;;;; print character in AL and add a carriage return if AL is a new line
+putc:
+        pusha
+.1:     xor     bh, bh
+        mov     ah, 0x0e
+        int     0x10            ; http://stanislavs.org/helppc/int_10-e.html
+        xor     dx, dx
+        mov     ah, 0x01
+.2:     int     0x14            ; http://stanislavs.org/helppc/int_14-1.html
+        test    ah, 0x80
+        jz      .3
+        mov     word [.2], 0x9090 ; turn `int 0x14` into NOPs
+.3:     cmp     al, 0x0a
+        jne     .4
+        mov     al, 0x0d
+        jmp     .1
+.4:     popa
+        ret
 
 ; `$` represents the current address, `$$` represents the address of the first
 ; instruction, so `$ - $$` is the number of bytes from the start to here
